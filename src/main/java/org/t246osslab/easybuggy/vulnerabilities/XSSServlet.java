22d8467e82ac3ead5e494ad8630e0ed8782f8cdf
que-bui <que-bui@dimage.co.jp>
2020-11-18 11:22:07 +0900

    fix xss

--- src/main/java/org/t246osslab/easybuggy/vulnerabilities/XSSServlet.java
+++ src/main/java/org/t246osslab/easybuggy/vulnerabilities/XSSServlet.java
@@ -3,6 +3,7 @@ package org.t246osslab.easybuggy.vulnerabilities;
 import java.io.IOException;
 import java.util.Locale;
 
+import java.text.Normalizer;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,10 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.t246osslab.easybuggy.core.servlets.AbstractServlet;
 
+import java.util.regex.Pattern;
+
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = { "/xss" })
 public class XSSServlet extends AbstractServlet {
-
+    
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
@@ -32,10 +35,12 @@ public class XSSServlet extends AbstractServlet {
             bodyHtml.append("<br><br>");
             bodyHtml.append("<input type=\"submit\" value=\"" + getMsg("label.submit", locale) + "\">");
             bodyHtml.append("<br><br>");
-
+      
+            
             if (!StringUtils.isBlank(string)) {
                 // Reverse the given string
                 String reversedName = StringUtils.reverse(string);
+                reversedName = stripXSSAttack(reversedName);
                 bodyHtml.append(getMsg("label.reversed.string", locale) + " : "
                         + reversedName);
             } else {
@@ -50,5 +55,27 @@ public class XSSServlet extends AbstractServlet {
         } catch (Exception e) {
             log.error("Exception occurs: ", e);
         }
+    }
+    private static Pattern[] patterns = new Pattern[]{
+            Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL),
+            Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL)
+        };
+    private String stripXSSAttack(String value) {
+        if (value != null) {
+            value = value.replaceAll("\0", "");
+            for (Pattern scriptPattern : patterns){
+                value = scriptPattern.matcher(value).replaceAll("");
+            }
+        }
+        return value;
     }
 }
